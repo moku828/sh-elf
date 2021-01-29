1c5d57bcbdc58310600270d6a17c62f8b3d4054b
Masayuki Okumura <basashi@sannet.ne.jp>
2021-01-30 00:25:14 +0900

    Add latest gcc, newlib and qemu

--- build_images.bat
+++ build_images.bat
@@ -10,6 +10,12 @@ docker push moku828/gcc:3.4.5-sh-elf
 cd C:\test\sh-elf\newlib-1.14.0
 docker build -t moku828/newlib:1.14.0-sh-elf .
 docker push moku828/newlib:1.14.0-sh-elf
+cd C:\test\sh-elf\gcc-10.2.0
+docker build -t moku828/gcc:10.2.0-sh-elf .
+docker push moku828/gcc:10.2.0-sh-elf
+cd C:\test\sh-elf\newlib-4.1.0
+docker build -t moku828/newlib:4.1.0-sh-elf .
+docker push moku828/newlib:4.1.0-sh-elf
 cd C:\test\sh-elf\binutils-2.36-remote
 docker build -t moku828/binutils:2.36-sh-elf-remote .
 docker push moku828/binutils:2.36-sh-elf-remote
@@ -22,4 +28,13 @@ docker push moku828/gcc:3.4.5-sh-elf-remote
 cd C:\test\sh-elf\newlib-1.14.0-remote
 docker build -t moku828/newlib:1.14.0-sh-elf-remote .
 docker push moku828/newlib:1.14.0-sh-elf-remote
+cd C:\test\sh-elf\gcc-10.2.0-remote
+docker build -t moku828/gcc:10.2.0-sh-elf-remote .
+docker push moku828/gcc:10.2.0-sh-elf-remote
+cd C:\test\sh-elf\newlib-4.1.0-remote
+docker build -t moku828/newlib:4.1.0-sh-elf-remote .
+docker push moku828/newlib:4.1.0-sh-elf-remote
+cd C:\test\sh-elf\qemu-5.2.0
+docker build -t moku828/qemu:5.2.0-sh4eb-softmmu .
+docker push moku828/qemu:5.2.0-sh4eb-softmmu
 pause
